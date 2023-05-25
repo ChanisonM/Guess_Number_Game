21d18868e93335a63c7fdd6cfb5faf98bb74f378
Chanison Aupathin <89581992+ChanisonM@users.noreply.github.com>
2023-05-25 22:22:58 +0700

    chanison

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const hint = document.getElementById("hint")
+const noOfGuessesRef = document.getElementById("on-of-guesses")
+const guessesNumberRef = document.getElementById("guessed-nums")
+const restartButton = document.getElementById("restart")
+const game = document.getElementById("game")
+const guessInput = document.getElementById("guess")
+const checkButton = document.getElementById("check-btn")
+
+let answer , noOFGuesses , guessedNumberArr ;
+
+const play = () =>{
+    
+}
